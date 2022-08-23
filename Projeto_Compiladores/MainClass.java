6994d2c0932d6a023f6aa75ef105df4390529a6c
Bruno Vieira <bruninho_vrs@hotmail.com>
2022-08-22 21:03:45 -0300

    Add files via upload

---
+++ Projeto_Compiladores/MainClass.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class MainClass{ 
+  public static void main(String args[]){
+       Scanner _key = new Scanner(System.in);
+double  a;
+a = 2.06;
+System.out.println(a);
+  }}
